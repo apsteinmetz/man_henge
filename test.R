c57239fe073389fd2d30ff61d8cde09dc548afa0
Art Steinmetz <apsteinmetz@yahoo.com>
2022-11-14 11:40:06 -0500

    test render of lidar

--- test.R
+++ test.R
@@ -14,23 +14,37 @@ library(jpeg)
 
 
 # Manhattan west from 19th to 26th
-# best view of Manhattanhenge is down 23rd
-#henge_extent <- extent(c(981000,992000,207000,214000))
+# best view of Manhattanmidtown is down 23rd
+#midtown_extent <- extent(c(981000,992000,207000,214000))
 
 # mini to test
-henge_extent <- extent(c(981000,983000,211000,213000))
-henge_bbox <- bbox(henge_extent)
+midtown_extent_west <- extent(c(981000,983000,211000,213000))
+midtown_extent <- extent(c(981000,992000,206000,213000))
+midtown_bbox <- bbox(midtown_extent)
+new_crs <- "+proj=longlat +datum=WGS84 +no_defs"
 
 
 #elev_file = "data/lower_man_lidar.tif"
-elev_file = "data/nyc_lidar_23rd.tif"
+elev_file = "LIDAR/hh_NYC_025.tif"
 elev_img <- raster::raster(elev_file) %>%
-   crop(extent(henge_extent)) %>%
-#  raster::aggregate(fact = 1) %>%
+   crop(extent(midtown_extent)) %>%
+   raster::aggregate(fact = 3) %>%
   {.}
 
 # get new extent for map overlay
-latlon_extent <- projectRaster(elev_img,crs = longlat()) %>%
+# takes a long time because we reproject the whole data set
+# latlon_extent <- projectRaster(elev_img,crs = longlat()) %>%
+#  extent()
+
+#create dummy raster to get converted extent
+# fast because we reproject an empty data set
+latlon_extent <- projectRaster(raster(
+  midtown_extent,
+  crs = crs(elev_img),
+  nrow = nrow(elev_img),
+  ncol = ncol(elev_img)
+),
+crs = new_crs) %>%
   extent()
 
 
@@ -39,7 +53,8 @@ man_lr <- c(latlon_extent@ymin,latlon_extent@xmin)
 man_ul <- c(latlon_extent@ymax,latlon_extent@xmax)
 map_overlay <- OpenStreetMap::openmap(man_ul,man_lr,
                                       minNumTiles = 12,
-                                      type = "bing",
+                                      #type = "bing",
+                                      type = "osm",
                                       mergeTiles = TRUE)
 
 map_ras <- raster(map_overlay)
@@ -65,7 +80,7 @@ elmat[elmat < 0] <- 0
 
 elmat %>%
   sphere_shade(texture = "bw") %>%
-#  add_overlay(overlay,
+ # add_overlay(overlay,
 #              alphalayer = .8) %>%
   rayshader::plot_map(elmat,
                       rotate = 0)
@@ -74,8 +89,8 @@ elmat %>%
  elmat %>%
    sphere_shade(texture = "bw") %>%
    add_water(detect_water(elmat)) %>%
-#   add_overlay(overlay,
-#               alphalayer = .8) %>%
+   add_overlay(overlay,
+               alphalayer = .8) %>%
    plot_3d(elmat,zscale = 5)
 
  # render_highquality()
